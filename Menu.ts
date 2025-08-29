8cefda7f8772e6a053a728dc0fcf1e7cdfc922fa
K4rin <karinecris.mix@gmail.com>
2025-08-29 12:28:57 -0300

    Subindo primeira parte, com menu e cores

---
+++ Menu.ts
@@ -0,0 +1,95 @@
+import { colors } from './src/util/Colors';
+import leia = require('readline-sync');
+
+
+export function main(){
+    let opcao: number;
+    let continuar: boolean =true;
+
+    while(continuar){
+        console.log(colors.bg.black, colors.fg.yellow,
+                    "*****************************************************");
+        console.log("                                                     ");
+        console.log("                  ORION BANK                         ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("            1 - Criar Conta                          ");
+        console.log("            2 - Listar todas as Contas               ");
+        console.log("            3 - Buscar Conta por Numero              ");
+        console.log("            4 - Atualizar Dados da Conta             ");
+        console.log("            5 - Apagar Conta                         ");
+        console.log("            6 - Sacar                                ");
+        console.log("            7 - Depositar                            ");
+        console.log("            8 - Transferir valores entre Contas      ");
+        console.log("            9 - Sair                                 ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ", colors.reset);
+
+        console.log("Entre com a opção desejada: ");
+        opcao = leia.questionInt("");
+
+        
+        switch(opcao){
+            case 1:
+                console.log(colors.fg.whitestrong,'\n\nCriar conta\n\n', colors.reset);
+                 keyPress()
+            break;
+            case 2:
+                console.log(colors.fg.whitestrong,'\n\nAqui esta a lista das sua contas:\n\n',colors.reset);
+                 keyPress()
+            break;
+            case 3:
+                console.log(colors.fg.whitestrong,'Consultar dados da conta - por numero.', colors.reset);
+                 keyPress()
+            break;
+            case 4:
+                console.log(colors.fg.whitestrong,'\n\nAtualizar dados da conta.\n\n', colors.reset);
+                 keyPress()
+            break;
+            case 5:
+                console.log(colors.fg.whitestrong,'\n\nApagar uma conta.\n\n', colors.reset);
+                 keyPress()
+            break;
+            case 6:
+                console.log(colors.fg.whitestrong,'\n\nSaque\n\n', colors.reset);
+                 keyPress()
+            break;
+            case 7:
+                console.log(colors.fg.whitestrong,'\n\nDepósito\n\n', colors.reset);
+                 keyPress()
+            break;
+            case 8:
+                console.log(colors.fg.whitestrong,'\n\nTranferencia entre contas\n\n', colors.reset);
+                 keyPress()
+            break;
+             case 9:
+            console.log(colors.fg.greenstrong,"\nOrion Bank - Inovação que ilumina o seu futuro.");
+            sobre();
+            console.log(colors.reset, "");
+            // process.exit(0);
+            continuar= false;
+            break;
+            default:
+                console.log(colors.fg.whitestrong,'\nOpção inválida!\n', colors.reset);
+                 keyPress()
+            break;
+        }
+
+    }
+}
+
+export function sobre(): void{
+    console.log("\n*****************************************************");
+    console.log("Projeto Desenvolvido por: ");
+    console.log("Karine Santos - karinecrislopes08@gmail.com");
+    console.log("github.com/k4rin");
+    console.log("*****************************************************");
+}
+function keyPress(): void {
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    leia.prompt();
+}
+main();
