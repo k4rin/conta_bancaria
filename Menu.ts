38cc5ac4d70fd084871a03cf627772eb39723f83
Karine Santos <112910735+k4rin@users.noreply.github.com>
2025-09-01 15:35:31 -0300

    Update Menu.ts

--- Menu.ts
+++ Menu.ts
@@ -1,10 +1,38 @@
 import { colors } from './src/util/Colors';
 import leia = require('readline-sync');
+import { Conta } from './src/model/Conta';
+import { ContaCorrente } from './src/model/ContaCorrente';
+import { ContaPoupanca } from './src/model/ContaPoupanca';
 
 
 export function main(){
     let opcao: number;
     let continuar: boolean =true;
+
+    // Objeto da Classe Conta (Teste)
+    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
+    conta.visualizar();
+    conta.sacar(10500);
+    conta.visualizar();
+    conta.depositar(5000);
+    conta.visualizar();
+
+   // Objeto da Classe ContaCorrente (Teste)
+    const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
+    contacorrente.visualizar();
+    contacorrente.sacar(2000);
+    contacorrente.visualizar();
+    contacorrente.depositar(1000);
+    contacorrente.visualizar();
+
+    // Objeto da Classe ContaPoupanca (teste)
+    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
+    contapoupanca.visualizar();
+    contapoupanca.sacar(200);
+    contapoupanca.visualizar();
+    contapoupanca.depositar(1000);
+    contapoupanca.visualizar();
+
 
     while(continuar){
         console.log(colors.bg.black, colors.fg.yellow,
